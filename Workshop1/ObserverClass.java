68d0c017821b2382cbf249be4992d79f64af98c3
William <williamjellett@gmail.com>
2017-08-09 00:17:22 +1000

    Add Observer Class, update ObserverInterface

---
+++ Workshop1/ObserverClass.java
@@ -0,0 +1,27 @@
+/* Author: William Ellett
+StudentID: 586703
+Last Modified: 9/08/2017
+Software Modelling and Design SWEN30006
+
+Description: implementation of the observer interface
+*/
+
+
+public class ObserverClass implements ObserverInterface{
+  private int observerID;
+
+  public void notify(EventClass stateChange){
+    System.out.printf("Observed state change from %d to %d%n", stateChange.getPreviousState(), stateChange.getState());
+  }
+
+  //Accessors
+  public int getObserverID(){
+    return observerID;
+  }
+
+  //Mutators
+  public void setObserverID(int observerID){
+    this.observerID = observerID;
+  }
+
+}
--- Workshop1/ObserverInterface.java
+++ Workshop1/ObserverInterface.java
@@ -1,11 +1,12 @@
 /* Author: William Ellett
 Student ID: 586703
-Last Modified: 8/8/2017
+Last Modified: 9/8/2017
 Software Modelling and Design SWEN30006
 
 Description: Interface for a basic implementation of the Observer Pattern
 */
 public interface ObserverInterface{
-  public void notify(EventClass event);
+  public void notify(EventClass observer);
   public int getObserverID();
+  public void setObserverID(int observerID);
 }
