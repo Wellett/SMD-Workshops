e172f2c22e4e599dea783d8d6ea74ea3f9b32f03
William <williamjellett@gmail.com>
2017-08-08 23:20:17 +1000

    completed matchObserver, removeObserver and deregisterObserver methods in SubjectClass

--- Workshop1/SubjectClass.java
+++ Workshop1/SubjectClass.java
@@ -11,7 +11,7 @@ public class SubjectClass implements SubjectInterface{
   private static final int MAX_SUBSCRIBERS = 100;
   private int state;
   private int numObservers;
-  private ObserverInterface[] obsevers;
+  private ObserverInterface[] observers;
   private int newID;
 
   //Constructors
@@ -19,7 +19,7 @@ public class SubjectClass implements SubjectInterface{
     state = 0;
     numObservers = 0;
     observers = new ObserverInterface[MAX_SUBSCRIBERS];
-    newID = 1;
+    newID = 0;
   }
 
   //Accessors
@@ -32,16 +32,37 @@ public class SubjectClass implements SubjectInterface{
   public int registerObserver(ObserverInterface newObserver){
     observers[numObservers] = newObserver;
     numObservers++;
+    newID ++;
     return newID;
   }
 
   //ooh this bit is actually a bit difficult...
+  //remove observer from observer database
   public void derigisterObserver(ObserverInterface deregObserver){
-
     for (int i = 0; i < numObservers; i++){
-      
+      if(matchObserver(deregObserver, observers[i])){
+        removeObserver(i);
+        break;
+      }
     }
   }
 
+  //Checks to see if two observers have matching ID
+  private boolean matchObserver(ObserverInterface observer1, ObserverInterface
+  observer2){
+    if (observer1.getObserverID() == observer2.getObserverID()){
+      return true;
+    }
+    return false;
+  }
+
+  //Does not necessarily erase observer, only rewrites by shifting objects
+  //in array
+  private void removeObserver(int index){
+    for (int i = index; i < numObservers; i++){
+      observers[i] = observers[i + 1];
+    }
+    numObservers--;
+  }
 
 }
