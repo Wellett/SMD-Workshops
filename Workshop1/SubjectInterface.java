009227cb51f1448cb5d273cd6fc41c8489b137c2
William <williamjellett@gmail.com>
2017-08-08 23:42:40 +1000

    update SubjectInterface

--- Workshop1/SubjectInterface.java
+++ Workshop1/SubjectInterface.java
@@ -9,4 +9,5 @@ Description: Subject interface for the observer pattern
 public interface SubjectInterface{
   public int registerObserver(ObserverInterface observer);
   public void derigisterObserver(ObserverInterface observer);
+  public void notifyAll(EventClass event);
 }
