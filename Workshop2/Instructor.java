8909cc6b1050166b6d08ea67dd7e5adad4ed7fc0
William <williamjellett@gmail.com>
2017-08-14 21:33:34 +1000

    add Instructor class, compiled files

---
+++ Workshop2/Instructor.java
@@ -0,0 +1,34 @@
+/* Author: William Ellett
+StudentID: 586703
+Last Modified: 14/8/2017
+Software Modelling and Design SWEN30006
+
+Description:
+*/
+
+public class Instructor{
+  private String name;
+  private int staffID;
+  private String position;
+
+  // Constructor
+  public Instructor(String name, int staffID, String position){
+    this.name = name;
+    this.staffID = staffID;
+    this.position = position;
+  }
+
+  //accessors
+  public String getName(){
+    return name;
+  }
+
+  public int getStaffID(){
+    return staffID;
+  }
+
+  public String getPosition(){
+    return position;
+  }
+
+}
