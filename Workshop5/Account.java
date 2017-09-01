8f279e7a1f6fa4890074a761c7aab076d95ef967
Wellett <williamjellett@gmail.com>
2017-09-01 14:04:06 +1000

    Add all state transitions to Account Class

--- Workshop5/Account.java
+++ Workshop5/Account.java
@@ -8,13 +8,79 @@ Workshop 5 Implementation Account Class
 */
 
 public class Account{
+
   private AccountState state;
   private String name;
 
+//Constructor
   public Account(String name){
     this.name = name;
-    AccountState = PENDING;
+    this.state = AccountState.PENDING;
   }
+
+  //MUTATORS
+  //state transitions
+
+  //approval
+  public void approval(){
+    if ((state == AccountState.PENDING)||state == AccountState.CLOSED){
+      state = AccountState.ACTIVE;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
+  //complaint
+  public void complaint(){
+    if (state == AccountState.ACTIVE){
+      state = AccountState.PRIORITY;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
+  //resolve complaint
+  public void resolveComplaint(){
+    if (state == AccountState.PRIORITY){
+      state = AccountState.ACTIVE;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
+  //fail payment
+  public void failPayment(){
+    if (state == AccountState.ACTIVE){
+      state = AccountState.DEFAULT;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
+  //pay all bills
+  public void payAllBills(){
+    if (state == AccountState.DEFAULT){
+      state = AccountState.ACTIVE;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
+  //close Account
+  public void closeAccount(){
+    if (state == AccountState.DEFAULT){
+      state = AccountState.CLOSED;
+    }
+    else{
+      System.out.println("Invalid state change");
+    }
+  }
+
 
 
 }
